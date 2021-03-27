9054cfe706a78d7f6ea6be24327189cc47309084
Al Piepho <alpiepho@yahoo.com>
2021-03-26 19:44:15 -0600

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -108,12 +108,12 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
-"index.html",
-"assets/NOTICES",
-"assets/AssetManifest.json",
-"assets/FontManifest.json"];
+  "https://alpiepho.github.io/unit_caclulator/",
+"https://alpiepho.github.io/unit_caclulator/main.dart.js",
+"https://alpiepho.github.io/unit_caclulator/index.html",
+"https://alpiepho.github.io/unit_caclulator/assets/NOTICES",
+"https://alpiepho.github.io/unit_caclulator/assets/AssetManifest.json",
+"https://alpiepho.github.io/unit_caclulator/assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
